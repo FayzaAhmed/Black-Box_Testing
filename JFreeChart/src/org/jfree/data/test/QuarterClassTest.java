2ac5dabac9b701a60691c3b7990406934f79c90e
FayzaAhmed <79043448+FayzaAhmed@users.noreply.github.com>
2023-04-16 23:24:26 +0200

    2nd push

--- JFreeChart/src/org/jfree/data/test/QuarterClassTest.java
+++ JFreeChart/src/org/jfree/data/test/QuarterClassTest.java
@@ -1,15 +1,15 @@
 package org.jfree.data.test;
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
+import java.util.Date;
 import java.time.Year;
-
 import org.jfree.data.time.Quarter;
 import org.junit.Test;
 
 
 public class QuarterClassTest {
     Quarter quarter;
-    Year year;
+
+    // Testing Default Constructor
     @Test
     public void testDefaultConstructor() {
         quarter = new Quarter();
@@ -18,8 +18,9 @@ public class QuarterClassTest {
     }
 
 
+    // Testing Parameterized Constructor Quarter(int quarter, int year)
     @Test
-    public void testParameterizedConstructor() {
+    public void testParameterizedConstructorWithValidQuarterAndYear() {
 
         int quart = 2;
         int year = 2023;
@@ -29,6 +30,40 @@ public class QuarterClassTest {
         assertEquals(quart, quarter.getQuarter(), 1E-13);
         assertEquals(year, quarter.getYear().getYear(), 1E-13);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParameterizedConstructorWithInvalidQuarterLower() {
+        quarter = new Quarter(0, 1900);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParameterizedConstructorWithInvalidQuarterUpper() {
+        quarter = new Quarter(5, 1900);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParameterizedConstructorWithInvalidYearLower() {
+        quarter = new Quarter(1, 1800);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParameterizedConstructorWithInvalidYearUpper() {
+        quarter = new Quarter(1, 10000);
+    }
+   
+
+    @Test
+    public void testParameterizedConstructorWithValidDate() {
+        quarter = new Quarter(new Date(1640995200000L)); // January 1, 2022
+        assertEquals(1, quarter.getQuarter(), 1E-13);
+        //assertNotNull(quarter);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParameterizedConstructorWithInvalidDate() {
+        quarter = new Quarter(new Date(0L)); // January 1, 1970
+    }
+
 
    /*
     @Test
@@ -47,3 +82,19 @@ public class QuarterClassTest {
     }*/
     
 }
+
+
+
+
+ /* 
+    @Test
+    public void testConstructorWithValidQuarterAndYear() {
+        Quarter quarter = new Quarter(1, Year.of(2022));
+        assertNotNull(quarter);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithInvalidQuarterAndYear() {
+        Quarter quarter = new Quarter(5, Year.of(2022));
+    }
+*/
