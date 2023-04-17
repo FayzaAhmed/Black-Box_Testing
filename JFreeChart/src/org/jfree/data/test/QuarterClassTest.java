b68bed703c017bff7737416241dde7c375c6ee5e
FayzaAhmed <79043448+FayzaAhmed@users.noreply.github.com>
2023-04-17 15:13:38 +0200

    3rd push

--- JFreeChart/src/org/jfree/data/test/QuarterClassTest.java
+++ JFreeChart/src/org/jfree/data/test/QuarterClassTest.java
@@ -50,8 +50,239 @@ public class QuarterClassTest {
     public void testParameterizedConstructorWithInvalidYearUpper() {
         quarter = new Quarter(1, 10000);
     }
-   
 
+    // Testing Parameterized Constructor Quarter(java.util.Date time)
+    // Testing Parameterized Constructor Quarter(java.util.Date time, java.util.TimeZone zone)
+    // Testing Parameterized Constructor Quarter(int quarter, Year year)
+
+    // Testing function compareTo(java.lang.Object o1)
+    @Test
+    public void testCompareToBefore()  //Same year with different quarter values
+    {
+        quarter = new Quarter(2, 2023);
+        Quarter obj = new Quarter(3, 2023);
+        int result = quarter.compareTo(obj);
+        assertTrue(result < 0);
+    }
+
+    @Test
+    public void testCompareToBefore2() //different year values
+    {
+        quarter = new Quarter(3, 2022);
+        Quarter obj = new Quarter(2, 2023);
+        int result = quarter.compareTo(obj);
+        assertTrue(result < 0);
+    }
+
+    @Test
+    public void testCompareToEqual() 
+    {
+        quarter = new Quarter(2, 2022);
+        Quarter obj = new Quarter(2, 2022);
+        int result = quarter.compareTo(obj);
+        assertTrue(result == 0);
+    }
+
+    @Test
+    public void testCompareToAfter() //Same year with different quarter values
+    {
+        quarter = new Quarter(2, 2023);
+        Quarter obj = new Quarter(1, 2023);
+        int result = quarter.compareTo(obj);
+        assertTrue(result > 0);
+    }
+
+    @Test
+    public void testCompareToAfter2() //different year values
+    {
+        quarter = new Quarter(2, 2023);
+        Quarter obj = new Quarter(1, 2022);
+        int result = quarter.compareTo(obj);
+        assertTrue(result > 0);
+    }
+
+    // Testing function equals(java.lang.Object obj)
+    @Test
+    public void testEqualValidQuarterAndYear() 
+    {
+        quarter = new Quarter(2, 2023);
+        Quarter obj = new Quarter(2, 2023);
+        boolean result = quarter.equals(obj);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testEqualInvalidQuater() 
+    {
+        quarter = new Quarter(2, 2023);
+        Quarter obj = new Quarter(3, 2023);
+        boolean result = quarter.equals(obj);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testEqualInvalidYear() 
+    {
+        quarter = new Quarter(2, 2023);
+        Quarter obj = new Quarter(2, 2022);
+        boolean result = quarter.equals(obj);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testEqualInvalidQuaterAndYear() 
+    {
+        quarter = new Quarter(2, 2023);
+        Quarter obj = new Quarter(3, 2022);
+        boolean result = quarter.equals(obj);
+        assertFalse(result);
+    }
+
+
+    // Testing function getFirstMillisecond(java.util.Calendar calendar)
+
+    // Testing function getLastMillisecond(java.util.Calendar calendar)
+
+    // Testing function getQuarter()
+    @Test
+    public void testGetQuarter() {
+        quarter = new Quarter(2, 2023);
+        assertEquals(2, quarter.getQuarter(), 1E-13);
+    }
+
+    // Testing function getSerialIndex()
+
+    // Testing function getYear()
+    
+    // Testing function hashCode()
+
+    // Testing function next()
+    @Test
+    public void testNext() //same year
+    {
+        quarter = new Quarter(2, 2023);
+        Quarter nextQuarter = (Quarter) quarter.next();
+        assertEquals(new Quarter(3, 2023), nextQuarter);
+    }
+
+    @Test
+    public void testNext2() //next quarter in different year
+    {
+        quarter = new Quarter(4, 2022);
+        Quarter nextQuarter = (Quarter) quarter.next();
+        assertEquals(new Quarter(1, 2023), nextQuarter);
+    }
+
+    @Test
+    public void testNextNull()
+    {
+        quarter = new Quarter(4, 9999);
+        assertNull(quarter.next());
+    }
+
+    /*******************************************************************************************/
+    // Testing function parseQuarter(java.lang.String s)
+    @Test
+    public void testParseQuarter() 
+    {
+        // Test case 1: Valid input string format "YYYY-QN" with dash
+        assertEquals(new Quarter(2, 2022), Quarter.parseQuarter("2022-Q2"));
+    
+        // Test case 2: Valid input string format "QN-YYYY" with dash
+        assertEquals(new Quarter(2, 2022), Quarter.parseQuarter("Q2-2022"));
+
+        // Test case 3: Valid input string format "YYYY-QN" with space
+        assertEquals(new Quarter(2, 2022), Quarter.parseQuarter("2022 Q2"));
+    
+        // Test case 4: Valid input string format "QN-YYYY" with space
+        assertEquals(new Quarter(2, 2022), Quarter.parseQuarter("Q2 2022"));
+
+        // Test case 5: Valid input string format "YYYY-QN" with comma
+        assertEquals(new Quarter(2, 2022), Quarter.parseQuarter("2022,Q2"));
+    
+        // Test case 6: Valid input string format "QN,YYYY" with comma
+        assertEquals(new Quarter(2, 2022), Quarter.parseQuarter("Q2,2022"));
+
+        // Test case 7: Valid input string format "YYYY-QN" with forward-slash
+        assertEquals(new Quarter(2, 2022), Quarter.parseQuarter("2022/Q2"));
+    
+        // Test case 8: Valid input string format "QN-YYYY" with forward-slash
+        assertEquals(new Quarter(2, 2022), Quarter.parseQuarter("Q2/2022"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParseQuarterInvalidInputs() 
+    {
+        // Test case 1: Invalid input string (missing quarter)
+        Quarter.parseQuarter("2022");        
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testParseQuarterInvalidInputs2() 
+    {
+        // Test case 2: Invalid input string (missing year)
+        Quarter.parseQuarter("Q2");        
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testParseQuarterInvalidInputs3() 
+    {
+        // Test case 3: Invalid input string (invalid quarter)
+        Quarter.parseQuarter("Q5-2022");        
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testParseQuarterInvalidInputs4() {
+        
+        // Test case 4: Invalid input string (invalid year)
+        Quarter.parseQuarter("Q2-ABC");        
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParseQuarterInvalidInputs5() {
+        
+        // Test case 5: Invalid input string (empty string)
+        Quarter.parseQuarter("");        
+    }
+
+    /*******************************************************************************************/
+    // Testing function previous()
+    @Test
+    public void testPrevious() //same year
+    {
+        quarter = new Quarter(2, 2023);
+        Quarter previousQuarter = (Quarter) quarter.previous();
+        assertEquals(new Quarter(1, 2023), previousQuarter);
+    }
+
+    @Test
+    public void testPrevious2() //previous quarter in different year
+    {
+        quarter = new Quarter(1, 2023);
+        Quarter previousQuarter = (Quarter) quarter.previous();
+        assertEquals(new Quarter(4, 2022), previousQuarter);
+    }
+
+    @Test
+    public void testPreviousNull()
+    {
+        quarter = new Quarter(1, 1900);
+        assertNull(quarter.previous());
+    }
+
+
+
+    // Testing function toString()
+    @Test
+    public void testToString() 
+    {
+        quarter = new Quarter(2, 2023);
+        assertEquals("Q2/2023", quarter.toString());
+    }
+
+
+
+    /*
     @Test
     public void testParameterizedConstructorWithValidDate() {
         quarter = new Quarter(new Date(1640995200000L)); // January 1, 2022
@@ -65,7 +296,7 @@ public class QuarterClassTest {
     }
 
 
-   /*
+   
     @Test
     public void testParameterizedConstructor2() {
 
